2911554a98347e56bf227817a3cfc57e1a12499f
gesari <sirus1257@gmail.com>
2026-02-23 14:40:21 -0500

    Updated _module.mjs to export Actor and Item bases, as well as System Model

--- modules/data/_module.mjs
+++ modules/data/_module.mjs
@@ -1,2 +1,11 @@
 // Current placeholder based on references from thymoran-dreams.mjs
 // Export modules as system defaults
+
+// Export Actors
+export {default as ThymoranDreamsActorBase } from './base-actor.mjs';
+
+// Export Items
+export { default as ThymoranDreamsItemBase } from './base-item.mjs';
+
+// System Model
+export { default as ThymoranDreamsSystemModel } from "./system-model.mjs";
