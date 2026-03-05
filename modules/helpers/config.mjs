5e1e02b980b79358d3d145e94ca04245bd382aa8
gesari <sirus1257@gmail.com>
2026-03-05 10:52:12 -0500

    Updated config.mjs based on boilerplate v13 template

--- modules/helpers/config.mjs
+++ modules/helpers/config.mjs
@@ -1,2 +1,18 @@
-// Current placeholder based on references from thymoran-dreams.mjs
-// export const THYMORANDREAMS
+export const THYMORANDREAMS = {};
+
+/**
+ * Taken from Boilerplate, which adds d&d ability scores at this stage
+ * e.g. STR, DEX, CON, etc.
+ * @type {Object}
+ */
+THYMORANDREAMS.stats = {
+    cog: 'THYMORANDREAMS.stat.cog.long',
+    for: 'THYMORANDREAMS.stat.for.long',
+    ins: 'THYMORANDREAMS.stat.ins.long',
+};
+
+THYMORANDREAMS.statAbbreviations = {
+    cog: 'THYMORANDREAMS.stat.cog.abbr',
+    for: 'THYMORANDREAMS.stat.for.abbr',
+    ins: 'THYMORANDREAMS.stat.ins.abbr',
+};
