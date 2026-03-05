7fe6b8fc5cc367f7eed3be7b3835326433f10a73
gesari <sirus1257@gmail.com>
2026-03-05 15:22:31 -0500

    Added item-feature.mjs based on boilerplate v13 template

---
+++ modules/data/item-feature.mjs
@@ -0,0 +1,8 @@
+import ThymoranDreamsItemBase from './base-item.mjs';
+
+export default class ThymoranDreamsFeature extends ThymoranDreamsItemBase {
+    static LOCALIZATION_PREFIXES = [
+        'THYMORANDREAMS.Item.base',
+        'THYMORANDREAMS.Item.Feature',
+    ];
+}
